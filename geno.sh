9a71073be2bc2ad8b0ddd51eed0bd521ad2c365b
Owenb135 <Jbaarck@gmail.com>
2026-04-25 20:15:52 -0400

    Update directory path in geno.sh

--- geno.sh
+++ geno.sh
@@ -1,5 +1,5 @@
 # shellcheck disable=SC2164
-cd "/home/owen0963/CLionProjects/CPP PROCJECT/build_linux"
+cd "/home/usr/CLionProjects/CPP PROCJECT/build_linux"
 ./MultiverseApp
 echo rano
 echo thanks
